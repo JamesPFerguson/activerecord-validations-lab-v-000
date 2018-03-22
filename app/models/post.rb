60e47bae01cb6cfb889f594326ded7d1ee8fa18a
James Ferguson <ncjamesferguson@gmail.com>
2018-03-22 17:50:49 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+  def is_clickbait?
+    if !self.title.include?("Won't Believe") || !self.title.include?("Secret") || !self.title.include?("Top") || !self.title.include?("Guess")
 end
